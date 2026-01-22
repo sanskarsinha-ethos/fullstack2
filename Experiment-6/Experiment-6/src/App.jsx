14c657f4d65117977b5afd030d735290a82752dc
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-01-22 10:32:30 +0530

    commitfix

--- Experiment-6/Experiment-6/src/App.jsx
+++ Experiment-6/Experiment-6/src/App.jsx
@@ -4,12 +4,17 @@ function Login() {
     const [password,setPassword]=useState("");
     const [userErr,setUserErr]=useState(false);
     const [passErr,setPassErr]=useState(false);
+    const [agreeTerms,setAgreeTerms]=useState(false);
 
   function loginHandle(e)
     {
         if(user.length<3 || password.length<3)
         {
             alert("type correct values")
+        }
+        else if(!agreeTerms)
+        {
+            alert("Please agree to the terms and conditions")
         }
         else
         {
@@ -47,12 +52,27 @@ function Login() {
         <div>
             <h1>Login</h1>
            <form onSubmit={loginHandle}>
-           <input type="text" placeholder="Enter User Id" onChange={userHandler} />{userErr?<span>User Not Valid</span>:""}
+           <input type="text" placeholder="Enter User Id" onChange={userHandler} value={user} />{userErr?<span style={{color: 'red'}}>User Not Valid</span>:""}
             <br /> <br />
-            <input type="password" placeholder="Enter User Password" onChange={passwordHandler}/>{passErr?<span>Password Not Valid</span>:""}
+            <input type="password" placeholder="Enter User Password" onChange={passwordHandler} value={password}/>{passErr?<span style={{color: 'red'}}>Password Not Valid</span>:""}
 
             <br /> <br />
-            <button type="submit"  >Login</button>
+            {!userErr && !passErr && user.length >= 3 && password.length >= 3 ? (
+                <div>
+                    <label>
+                        <input 
+                            type="checkbox" 
+                            checked={agreeTerms}
+                            onChange={(e)=>setAgreeTerms(e.target.checked)}
+                        />
+                        I agree 
+                    </label>
+                    <br /> <br />
+                    <button type="submit" disabled={!agreeTerms}>Login</button>
+                </div>
+            ) : (
+                <button type="submit" disabled>Login</button>
+            )}
            </form>
         </div>
     )
